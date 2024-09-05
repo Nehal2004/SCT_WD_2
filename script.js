564306df42faf0081cbea430413c680405bc3b37
Nehal2004 <nehaljain1709@gmail.com>
2024-09-05 13:39:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,78 +1,78 @@
-let startBtn = document.getElementById('start');
-let stopBtn = document.getElementById('stop');
-let resetBtn = document.getElementById('reset');
+let cells = document.querySelectorAll(".cell");
+const statusText = document.getElementsByClassName("statusText")[0];
+const restartBtn = document.getElementsByClassName("restartBtn")[0];
 
-let hour = 00;
-let minute = 00;
-let second = 00;
-let count = 00;
+let currentPlayer = "x";
+let gameIsOver = false;
+let winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-startBtn.addEventListener('click', function () {
-    timer = true;
-    stopWatch();
-});
+const updateStatusText = () => {
+  statusText.innerHTML = `Player ${currentPlayer}'s turn`;
+};
 
-stopBtn.addEventListener('click', function () {
-    timer = false;
-});
+const checkForWinner = () => {
+  winningConditions.forEach((combination) => {
+    let check = combination.every(
+      (index) => cells[index].innerHTML.trim() === currentPlayer
+    );
+    if (check) {
+      statusText.innerHTML = `${currentPlayer} won`;
+      gameIsOver = true;
 
-resetBtn.addEventListener('click', function () {
-    timer = false;
-    hour = 0;
-    minute = 0;
-    second = 0;
-    count = 0;
-    document.getElementById('hr').innerHTML = "00";
-    document.getElementById('min').innerHTML = "00";
-    document.getElementById('sec').innerHTML = "00";
-    document.getElementById('count').innerHTML = "00";
-});
-
-function stopWatch() {
-    if (timer) {
-        count++;
-
-        if (count == 100) {
-            second++;
-            count = 0;
-        }
-
-        if (second == 60) {
-            minute++;
-            second = 0;
-        }
-
-        if (minute == 60) {
-            hour++;
-            minute = 0;
-            second = 0;
-        }
-
-        let hrString = hour;
-        let minString = minute;
-        let secString = second;
-        let countString = count;
-
-        if (hour < 10) {
-            hrString = "0" + hrString;
-        }
-
-        if (minute < 10) {
-            minString = "0" + minString;
-        }
-
-        if (second < 10) {
-            secString = "0" + secString;
-        }
-
-        if (count < 10) {
-            countString = "0" + countString;
-        }
-
-        document.getElementById('hr').innerHTML = hrString;
-        document.getElementById('min').innerHTML = minString;
-        document.getElementById('sec').innerHTML = secString;
-        document.getElementById('count').innerHTML = countString;
-        setTimeout(stopWatch, 10);
+      combination.forEach((index) => {
+        cells[index].classList.add("winning-cell");
+        cells[index].style.color = "blue";
+      });
     }
-}
+  });
+};
+
+const checkForDraw = () => {
+  if (![...cells].some((cell) => cell.innerHTML.trim() === "") && !gameIsOver) {
+    statusText.innerHTML = "It's a draw!";
+    gameIsOver = true;
+  }
+};
+
+updateStatusText();
+
+cells = Array.from(cells);
+cells.forEach((cell) => {
+  cell.addEventListener("click", () => {
+    if (gameIsOver || cell.innerHTML.trim() !== "") return;
+    cell.innerHTML = currentPlayer;
+    checkForWinner();
+    checkForDraw();
+
+    if (!gameIsOver) {
+      currentPlayer = currentPlayer === "x" ? "o" : "x";
+      updateStatusText();
+    }
+  });
+});
+
+restartBtn.addEventListener("click", () => {
+  restartGame();
+});
+
+const restartGame = () => {
+  cells.forEach((cell) => {
+    cell.innerHTML = "";
+    cell.classList.remove("winning-cell");
+    cell.style.color = ""; 
+  });
+
+  currentPlayer = "x";
+  gameIsOver = false;
+
+  updateStatusText();
+};
